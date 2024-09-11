f7d5866f4c477f32e8cb231373d98bf2cbf93bb0
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-09-11 21:23:12 +0530

    Add files via upload

---
+++ Bpattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Bpattern
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of n:");
+		int n =sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0 &&j!=n-1||j==0&& i!=0 &&i!=n-1 ||j==n-1&&i!=0&&i!=n-1||i==n-1&&j!=n-1 ||i==n/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println( );
+		}
+	}
+}
